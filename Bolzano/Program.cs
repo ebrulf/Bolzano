998d5f38e1e20bcff84a17f420bd1892880be8ea
ebrulf <85363333+ebrulf@users.noreply.github.com>
2022-10-14 22:53:22 +0200

    Dodaj pliki projektów.

---
+++ Bolzano/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Plotly.NET;
+using Plotly.NET.LayoutObjects;
+
+Console.WriteLine("Hello, World!");
+//przykładowy kod
+//StreamReader w Systems.IO dołączony
+double[] x = new double[] { 1, 2 };
+double[] y = new double[] { 5, 10 };
+
+LinearAxis xAxis = new LinearAxis();
+xAxis.SetValue("title", "xAxis");
+xAxis.SetValue("showgrid", false);
+xAxis.SetValue("showline", true);
+
+LinearAxis yAxis = new LinearAxis();
+yAxis.SetValue("title", "yAxis");
+yAxis.SetValue("showgrid", false);
+yAxis.SetValue("showline", true);
+
+Layout layout = new Layout();
+layout.SetValue("xaxis", xAxis);
+layout.SetValue("yaxis", yAxis);
+layout.SetValue("showlegend", true);
+
+Trace trace = new Trace("scatter");
+trace.SetValue("x", x);
+trace.SetValue("y", y);
+trace.SetValue("mode", "markers");
+trace.SetValue("name", "Hello from C#");
+
+GenericChart
+    .ofTraceObject(true, trace)
+    .WithLayout(layout)
+    .Show();
