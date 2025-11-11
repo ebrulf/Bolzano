5913c24bed09075a4278a75f74e4e32ae2469e31
Konstanty Dąbrowski <konstantyd@o2.pl>
2025-11-11 23:52:54 +0100

    Pierwsze babranie w opcjach konsolowych oraz w testach plus wypisanie do pliku GraphML

--- Bolzano/Program.cs
+++ Bolzano/Program.cs
@@ -1,7 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.CommandLine;
+
 Console.WriteLine("Hello, World!");
 //przykładowy kod
 //StreamReader w Systems.IO dołączony
 double[] x = new double[] { 1, 2 };
 double[] y = new double[] { 5, 10 };
+// zważa na wielkość liter
+Option<bool> helpOption = new("--help", ["-h", "/h", "-?", "/?"]);
+helpOption.Aliases.Add("--pomoc");
+RootCommand rootCommand = new("Praca inżynierska");// i już nie trzeba doprecyzować typu
--- Testy/UnitTest1.cs
+++ Testy/UnitTest1.cs
@@ -1,16 +1,66 @@
-﻿namespace Testy
+﻿using QuikGraph;
+using QuikGraph.Serialization;
+using System.Xml;
+
+namespace Testy
 {
     public class Tests
     {
+        #region Grafowe
+        Edge<int>[] edges;
+        AdjacencyGraph<int, Edge<int>>? graph, graph2;
         [SetUp]
         public void Setup()
         {
+            edges = new[] { new Edge<int>(1, 2), new Edge<int>(0, 1) };
+            graph = edges.ToAdjacencyGraph<int, Edge<int>>();
+            graph2 = null;
+            Console.WriteLine("Wykonuję testy");
         }
 
         [Test]
-        public void Test1()
+        public void PiszGraf()
         {
-            Assert.Pass();
+            foreach (var vertex in graph.Vertices)
+            {
+                Console.WriteLine(vertex);
+            }
+            foreach (var edge in graph.Edges)
+            {
+                Console.WriteLine(edge);
+            }
+            Assert.Pass("Udało się wypisać");
         }
+        [Test]
+        public void SprawdźWymiaryGrafu()
+        {
+            Assert.That(edges, Has.Length.EqualTo(2));//można dać wincyj Assertów
+        }
+        [Test(ExpectedResult = 3)]
+        public int LiczbaWierzchołków()
+        {
+            return graph.VertexCount;
+        }
+        [Test]
+        public void Zapisz()
+        {
+            using (var xmlWriter = XmlWriter.Create("graf.xml"))
+            {
+                graph.SerializeToGraphML<int,Edge<int>, AdjacencyGraph<int, Edge<int>>>(xmlWriter);
+            }
+        }
+        [Test]
+        public void Wczytaj()
+        {
+            using (var xmlReader = XmlReader.Create("graf.xml"))
+            {
+                graph2.DeserializeFromGraphML(
+                    xmlReader,
+                    id => int.Parse(id),
+                    (source, target, id) => new Edge<int>(source, target));//uchu nie wczytuje
+            }
+            Assert.That(graph2, Is.EqualTo(graph));
+        }
+        #endregion
     }
 }
